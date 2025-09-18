cb89ee1198a4c36e86618e842723dff3e0b8dc11
Izadora2ds2025 <steclan.izadora@escola.pr.gov.br>
2025-09-18 10:28:19 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -8,13 +8,17 @@ class Aula05  extends Component {
         <View style={ styles.caixa1 }>1</View>
         <View style={ styles.caixa2 }>
           <Image 
-          style={styles.img1 }
-          source={{ uri: 'https://www.estadao.com.br/resizer/v2/KJDE6VL4F5AVJJJBLH5XYGVWD4.png?quality=80&auth=9298eb03c61f9b7995df9a8c47b7eed8ca9e118c532d97b464527bc3286740b8&width=380'}}
+          style={ styles.img1 }
+          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3Zn3yjeyCM1g3FjNXh7DMTRgiT6OGHjh5g&s' }}
+          />
+          <Image 
+          source={ require('../img/teste.webp')}
+          style={ styles.img2 }
           />
         </View>
         <View style={ styles.caixa3 }>
-        <Image
-        />
+         < Image/>
+        </View>
       </View>
     );
   }
@@ -28,39 +32,40 @@ const styles = StyleSheet.create({
      backgroundColor: 'white',
   },
 
-
   caixa1: {
     height: 100,
     backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
     color: 'white',
-    fontSize: 20
+    fontSize: 20,
   },
-
 
   caixa2: {
    flex: 1,
    backgroundColor: 'green',
-   alignItems: 'center',
-   justifyContent: 'center',
-   alignItems: 'center'
-   
-   
+   color: 'white',
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
 },
-
   caixa3: {
    backgroundColor: 'blue',
    height: 100,
-   alignItems: 'center',
-   justifyContent: 'center',
-   color: 'white',
-   fontSize: 20
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 20
 },
+
 img1: {
   width: 50,
   height: 50,
-}
+},
+img2: {
+  width: 100,
+  height: 100,
+},
 
 })
 
